a37826e9e8793f51efdabf5797de75775a650c41
chinthalapudigayathri <gayathri.ch.sde@gmail.com>
2026-05-13 21:25:46 +0530

    rerouting urls

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { connectDB } from "./config/db";
 import { connectKafka } from "./config/kafka";
-
+//import routes
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
+
+
+// Register all routes
+app.use("/api", routes);
 
 // Initialize external services
 connectDB();
